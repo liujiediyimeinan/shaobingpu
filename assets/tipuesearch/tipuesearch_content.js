6bd7f40a398cdc44c2c17156b018b98774a78f05
六界第一美男瓜 <speechcontrol@stop.com>
2018-11-20 15:09:06 -0500

    。

--- assets/tipuesearch/tipuesearch_content.js
+++ assets/tipuesearch/tipuesearch_content.js
@@ -1,18 +1,83 @@
-
+---
+# Content index for Tipue Search
+# https://github.com/jekylltools/jekyll-tipue-search
+# v1.4
+layout: null
+---
+{%- assign index = "" | split: "" -%}
+{%- assign excluded_files = site.tipue_search.exclude.files -%}
+{%- assign excluded_tags = site.tipue_search.exclude.tags | uniq -%}
+{%- assign excluded_categories = site.tipue_search.exclude.categories | uniq -%}
+{%- assign excluded_taxonomies = excluded_tags | concat: excluded_categories | uniq -%}
+{%- for post in site.posts -%}
+  {%- unless post.exclude_from_search == true or excluded_files contains post.path -%}
+    {%- assign has_excluded_taxonomy = false -%}
+    {%- for tag in post.tags -%}
+      {%- if excluded_taxonomies contains tag -%}
+        {%- assign has_excluded_taxonomy = true -%}
+      {%- endif -%}
+    {%- endfor -%}
+    {%- for category in post.categories -%}
+      {%- if excluded_taxonomies contains category -%}
+        {%- assign has_excluded_taxonomy = true -%}
+      {%- endif -%}
+    {%- endfor -%}
+    {%- unless has_excluded_taxonomy == true -%}
+      {%- assign index = index | push: post | uniq -%}
+    {%- endunless -%}
+  {%- endunless -%}
+{%- endfor -%}
+{%- if site.tipue_search.include.pages == true -%}
+  {%- for page in site.html_pages -%}
+    {%- unless page.exclude_from_search == true or excluded_files contains page.path -%}
+      {%- assign has_excluded_taxonomy = false -%}
+      {%- for tag in page.tags -%}
+        {%- if excluded_taxonomies contains tag -%}
+          {%- assign has_excluded_taxonomy = true -%}
+        {%- endif -%}
+      {%- endfor -%}
+      {%- for category in page.categories -%}
+        {%- if excluded_taxonomies contains category -%}
+          {%- assign has_excluded_taxonomy = true -%}
+        {%- endif -%}
+      {%- endfor -%}
+      {%- unless has_excluded_taxonomy == true -%}
+        {%- assign index = index | push: page | uniq -%}
+      {%- endunless -%}
+    {%- endunless -%}
+  {%- endfor -%}
+{%- endif -%}
+{%- for collection in site.tipue_search.include.collections -%}
+  {%- assign documents = site.documents | where:"collection",collection -%}
+  {%- for document in documents -%}
+    {%- unless document.exclude_from_search == true or excluded_files contains document.path -%}
+      {%- assign has_excluded_taxonomy = false -%}
+      {%- for tag in document.tags -%}
+        {%- if excluded_taxonomies contains tag -%}
+          {%- assign has_excluded_taxonomy = true -%}
+        {%- endif -%}
+      {%- endfor -%}
+      {%- for category in document.categories -%}
+        {%- if excluded_taxonomies contains category -%}
+          {%- assign has_excluded_taxonomy = true -%}
+        {%- endif -%}
+      {%- endfor -%}
+      {%- unless has_excluded_taxonomy == true -%}
+        {%- assign index = index | push: document | uniq -%}
+      {%- endunless -%}
+    {%- endunless -%}
+  {%- endfor -%}
+{%- endfor -%}
 var tipuesearch = {"pages": [
-     {"title": "Tipue", "text": "", "tags": "jQuery HTML5 CSS", "url": "http://www.tipue.com"},
-     {"title": "Tipue Search, a site search engine jQuery plugin", "text": "Tipue Search is a site search engine jQuery plugin. It's free, open source, responsive and fast. Tipue Search only needs a browser that supports jQuery. It doesn't need MySQL or similar. In Static mode it doesn't even need a web server.", "tags": "JavaScript", "url": "http://www.tipue.com/search"},
-     {"title": "Tipue Search Documentation", "text": "Tipue Search is a site search engine jQuery plugin. It's free, open source and responsive. Tipue Search uses various modes for loading content. Static mode uses a JavaScript object, while JSON mode uses JSON. Live mode grabs content from a list of pages dynamically.", "tags": "docs", "url": "http://www.tipue.com/search/docs"},     
-     {"title": "Tipue Search Static mode demo", "text": "Tipue Search is a site search engine jQuery plugin. This is a demo of Static mode. Enter tipue into the search box above.", "tags": "", "url": "http://www.tipue.com/search/demos/static"},
-     {"title": "Tipue Search Live mode demo", "text": "Tipue Search is a site search engine jQuery plugin. This is a demo of Live mode. Enter tipue into the search box above.", "tags": "", "url": "http://www.tipue.com/search/demos/live"},
-     {"title": "Tipue jQuery plugins Support", "text": "If you're stuck we offer a range of flexible support plans for our jQuery plugins.", "tags": "", "url": "http://www.tipue.com/support"},
-     {"title": "Tipr, a small and simple jQuery tooltip plugin", "text": "Tipr is a small and simple jQuery tooltip plugin. It's free and open source. Tipr displays attractive tooltips, and it's a shade under 4KB, CSS included.", "tags": "JavaScript", "url": "http://www.tipue.com/tipr"},
-     {"title": "The Tipue blog", "text": "An occasional blog covering CSS, web development, etc.", "tags": "HTML5", "url": "http://www.tipue.com/?d=2"},
-     {"title": "About Tipue", "text": "Tipue is a small web development studio based in North London. We've been around for well over a decade. We design innovative add-ins, plugins, code and features with heavy-duty Perl, MySQL and jQuery.", "tags": "", "url": "http://www.tipue.com/about"},     
-     {"title": "The Tipue blog - The complete guide to centering a div", "text": "Every new developer inevitably finds that centering a div isn't as obvious as you'd expect. Centering what's inside a div is easy enough by giving the text-align property a value of center, but then things tend to get a bit sticky. When you get to centering a div vertically, you can end up in a world of CSS hurt.", "tags": "HTML", "url": "http://www.tipue.com/blog/center-a-div"},
-     {"title": "The Tipue blog - Native HTML5 autocomplete with input list", "text": "This article shows how with the HTML5 input list attribute and datalist element you can easily set up an input box with a custom autocomplete without jQuery, JavaScript or similar.", "tags": "", "url": "http://www.tipue.com/blog/input-list"},
-     {"title": "The Tipue blog - The really simple guide to z-index", "text": "The CSS z-index property often trips up new and even experienced developers. The aim of this article is to boil down a somewhat-complex specification to 3 major points, which should ease most z-index pain.", "tags": "", "url": "http://www.tipue.com/z-index"}
+{%- for document in index -%}
+  {%- assign tags = document.tags | uniq -%}
+  {%- assign categories = document.categories | uniq -%}
+  {%- assign taxonomies = tags | concat: categories | uniq -%}
+  {
+    "title": {{ document.title | smartify | strip_html | normalize_whitespace | jsonify }},
+    "text": {{ document.content | strip_html | normalize_whitespace | jsonify }},
+    "tags": {{ taxonomies | join: " " | normalize_whitespace | jsonify }},
+    "url": {{ document.url | relative_url | jsonify }}
+  }{%- unless forloop.last -%},{%- endunless -%}
+{%- endfor -%}
 ]};
-
-
-
